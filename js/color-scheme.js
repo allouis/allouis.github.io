387b81da9075002d9b2d08f43b581bcc6014a249
Fabien <fabien@allou.is>
2014-01-14 21:00:58 +0000

    Wrapping inclosure, exposing setHighlight, settign bg based on time, andding event listners for toggle btns

--- js/color-scheme.js
+++ js/color-scheme.js
@@ -1,67 +1,68 @@
-if(!localStorage.getItem("oldHighlightIndex")) {
-    localStorage.setItem("oldHighlightIndex", 1000);
-}
-var highlight = "yellow";
-var isLight = true;
-var highlights = [
-    "yellow",
-    "red",
-    "blue",
-    "green",
-    "cyan",
-    "orange",
-    "magenta",
-    "violet"
-];
-var html = document.querySelector("html");
-function switchBg(){
-    if(isLight){
-        html.classList.remove("light");
-        html.classList.add("dark");
-    } else {
-        html.classList.add("light");
-        html.classList.remove("dark");
+(function(){
+    if(!localStorage.getItem("oldHighlightIndex")) {
+        localStorage.setItem("oldHighlightIndex", 1000);
     }
-    isLight = !isLight;
-}
+    var highlight = "yellow";
+    var highlights = [
+        "yellow",
+        "red",
+        "blue",
+        "green",
+        "cyan",
+        "orange",
+        "magenta",
+        "violet"
+    ];
+    var html = document.querySelector("html");
 
-function setHighlight(color){
-    if(highlights.indexOf(color) < 0){
-        return false;
-    }
-    html.classList.remove(highlight);
-    html.classList.add(color);
-    highlight = color;
-    return true;
-}
-
-var changeBg = Math.round(Math.random());
-
-if (changeBg) {
-    switchBg();
-}
-
-function randomHighlights() {
-    var highlightIndex = getRandomHighlightIndex();
-    var oldIndex = localStorage.getItem("oldHighlightIndex");
-    if(highlightIndex === parseInt(oldIndex, 10)) {
-        return randomHighlights();
-    }
-    localStorage.setItem("oldHighlightIndex", highlightIndex);
-    setHighlight(highlights[highlightIndex]);
-}
-
-function getRandomHighlightIndex(){
-    return Math.floor(Math.random() * 7);
-}
-randomHighlights();
-
-var boxes = [].slice.call(document.querySelectorAll('.color-scheme .box'));
-
-boxes.forEach(function(box){
-    box.addEventListener('click', function(){
-        if (!setHighlight(box.classList[1])) {
-            switchBg();
+    function setHighlight(color){
+        if(highlights.indexOf(color) < 0){
+            return false;
         }
-    });
-});
+        html.classList.remove(highlight);
+        html.classList.add(color);
+        highlight = color;
+        return true;
+    }
+
+    function randomHighlights() {
+        var highlightIndex = random(highlights.length);
+        var oldIndex = localStorage.getItem("oldHighlightIndex");
+        if(highlightIndex === parseInt(oldIndex, 10)) {
+            return randomHighlights();
+        }
+        localStorage.setItem("oldHighlightIndex", highlightIndex);
+        setHighlight(highlights[highlightIndex]);
+    }
+
+    function random(i){
+        return Math.floor(Math.random() * i);
+    }
+
+    window.setHighlight = setHighlight;
+
+    var now = new Date();
+
+    var hour = now.getHours();
+
+    if (hour < 6 || hour > 18) {
+        html.classList.remove('light');
+        html.classList.add('dark');
+    }
+    randomHighlights();
+
+    document.querySelector('.bg-button.light')
+        .addEventListener('click', function(){
+            html.classList.remove('dark');
+            html.classList.add('light');
+        }
+    );
+
+    document.querySelector('.bg-button.dark')
+        .addEventListener('click', function(){
+            html.classList.remove('light');
+            html.classList.add('dark');
+        }
+    );
+
+}());
